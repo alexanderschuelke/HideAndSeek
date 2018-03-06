28a4a82f309cefdcd5505923425adb7e1335466d
Alexander Schülke <schuelke.alexander@gmail.com>
2018-03-06 21:22:27 +0100

    first commit

---
+++ HideAndSeek/HideAndSeek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideAndSeek
+//
+//  Created by Alexander Schülke on 06.03.18.
+//  Copyright © 2018 Alexander Schülke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
